fe9f412a3fba237a0dfbc0dbe489e60b45779a8f
karlavdelgadof <karlavdelgadof@gmail.com>
2022-12-16 16:09:41 -0500

    Add app logo img and update social icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import Footer from './Footernav';
+import pawLogo from '../multimedia/animal-care.png';
 
 const Navbar = () => (
 // const location = useLocation();
-  <nav className="navbar flex flex-col justify-around h-screen loat-left w-1/4">
-    <div className="brand text-center">
-      <div id="app-logo" />
+  <nav className="navbar flex flex-col justify-around h-screen float-left w-1/4 pt-4">
+    <div className="brand text-center flex flex-col items-center">
+      <div className=""><img src={ pawLogo } alt="Pawpal app logo"/></div>
       <h1 className="app-name">PawPal</h1>
     </div>
 
