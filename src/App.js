560861eaa8340ede486596727dbee4f778ae0f65
Karam Fouad <karam.ezz84@gmail.com>
2022-12-16 08:01:05 +0200

    App root component (#13)
    * Initialize react app
    
    * Add linter
    
    * Set up linters
    
    * Install react router
    
    * Install and setup tailwind css
    
    * Modify app title
    
    * Test app component and fix stylelint errors
    
    * Install MUI library for date picker field
    
    * Install redux and react-redux
    
    * Install redux toolkit
    
    * Install proptypes logger and thunk
    
    * Fix linter errors
    
    * Delete templet files
    
    * Add react router
    
    * Add routes for app.js file
    
    Co-authored-by: karlavdelgadof <karlavdelgadof@gmail.com>
    Co-authored-by: Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>This is the app component of Pawpal</h1>
+      <Routes>
+        <Route path="/" element="" />
+      </Routes>
     </div>
   );
 }
