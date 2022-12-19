b8687f1672eb3df6b43d79a34280f567657c041d
Rodrigo Johann Reckziegel Nunes <hannziegel@gmail.com>
2022-12-19 01:13:15 -0300

    Add userSlice

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
+import userReducer from './user/userSlice';
 // import nameReducer from './foldername/nameSlice';
 
 const store = configureStore({
   reducer: {
     // reducername: nameReducer,
+    user: userReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
 });
