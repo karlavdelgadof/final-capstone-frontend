46ad911920fe253958ea67dcd82932addd813d40
karlavdelgadof <karlavdelgadof@gmail.com>
2022-12-20 07:25:41 -0500

    Add provider import

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App';
 import store from './redux/configureStore';
