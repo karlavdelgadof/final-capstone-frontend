ce8bfff314d42608b053b48bd67bde802297c07b
karlavdelgadof <karlavdelgadof@gmail.com>
2022-12-21 14:35:01 -0500

    Uncomment reservations reducer

--- src/redux/Reservations/reservations.js
+++ src/redux/Reservations/reservations.js
@@ -1,5 +1,4 @@
-/* import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-// import accommodations from '../Accomodations/accommodations';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 // import axios from 'axios';
 
 // const apiURL = 'https://us-central1-bookstore-api-e63c8.cloudfunctions.net/bookstoreApi/apps/q4jqYH5waNwpAMlI8Ds9/books';
@@ -24,12 +23,12 @@ const initialState = [
 // return response.data;
 // });
 
-// export const getReservations = createAsyncThunk('reservations/getReservations', async () => {
+export const getReservations = createAsyncThunk('reservations/getReservations', async () => {
 // const response = await axios.get(apiURL);
 // console.log(response.data)
 // return response.data;
-// initialState);
-// });
+return initialState;
+});
 
 // export const cancelReservation = createAsyncThunk('reservations/cancelReservation',
 //   async (reservation) => {
@@ -43,22 +42,22 @@ export const reservationsSlice = createSlice({
   reducers: {},
   extraReducers: (builder) => {
     // Add reducers to handle loading state as needed
-    builder
+      builder
       // .addCase(reserve.fulfilled, (state, action) => {
-      //   // Create reservation
+      //   //Create reservation
       //   state.push(action.payload);
       // })
       .addCase(getReservations.fulfilled, (state, action) => Object.entries(action.payload).map(
         ([id, [reservation]]) => ({ ...reservation, id }),
-      ));
-    // .addCase(cancelReservation.fulfilled, (state, action) => {
-    //   state.forEach((reservation) => {
-    //     if (reservation.id === action.payload.id) {
-    //       state.splice(state.indexOf(reservation), 1);
-    //     }
-    //   });
-    // });
+      ))
+      // .addCase(cancelReservation.fulfilled, (state, action) => {
+      // state.forEach((reservation) => {
+      //     if (reservation.id === action.payload.id) {
+      //       state.splice(state.indexOf(reservation), 1);
+      //     }
+      //   });
+      // });
   },
-}); */
+});
 
-/* export default reservationsSlice.reducer; */
+export default reservationsSlice.reducer;
