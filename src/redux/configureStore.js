d8810c6c77f0ace988bbfe694589d6cd72dc0433
karlavdelgadof <karlavdelgadof@gmail.com>
2022-12-20 23:23:25 -0500

    Add reservations reducer to store

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
 import accommodationsReducer from './Accomodations/accommodations';
+import reservationsReducer from './Reservations/reservations';
 
 const store = configureStore({
   reducer: {
+    reservations: reservationsReducer,
     accommodations: accommodationsReducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
