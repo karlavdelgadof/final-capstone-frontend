b66cce1cb18b3ff04db8db4c0419186dbfcf1ef6
karlavdelgadof <karlavdelgadof@gmail.com>
2022-12-22 23:29:46 -0500

    Add background to reservations container

--- src/pages/MyReservations.js
+++ src/pages/MyReservations.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReservationsContainer from '../components/Reservations/ReservationsContainer';
 
 const MyReservations = () => (
-  <main className="h-full">
+  <main className="h-full reservations-bg">
     <div>
       <ReservationsContainer />
     </div>
