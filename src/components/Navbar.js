a571bf064742d1a08a7d5af5de389f2c7df46862
karlavdelgadof <karlavdelgadof@gmail.com>
2022-12-22 23:56:40 -0500

    Delete unused link

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -17,8 +17,6 @@ const Navbar = () => (
       <li className="p-4"><NavLink to="/reservations/new">Reserve</NavLink></li>
       {/* /reservations */}
       <li className="p-4"><NavLink to="/reservations">My Reservations</NavLink></li>
-      {/* /profile */}
-      <li className="p-4"><NavLink to="">My Profile</NavLink></li>
       {/* /accommodations/new */}
       <li className="p-4"><NavLink to="/accomodation/new">Add a House sit</NavLink></li>
       {/* /accommodations */}
